2b0a18d617b8133cad4724f0a7705afd7a34a35a
timur <timtimch123@gmail.com>
2026-03-19 20:25:22 +0300

    build refactor

--- nob.c
+++ nob.c
@@ -28,6 +28,8 @@ const char* src_dir;
 const char* csrc_dir;
 const char* prj_dir;
 
+Cmd cmd = {0};
+
 void get_paths()
 {
     android_home = getenv("ANDROID_HOME");
@@ -54,18 +56,16 @@ void get_paths()
 }
 
 // TODO: make incremental compilation with `nob_needs_rebuild` instead of full recompiling
-bool clear_build_files(Cmd* cmd)
+bool clear_build_files()
 {
-    Dir_Entry dir = {0};
-    if (dir_entry_open(BUILD_DIR, &dir)) {
-        dir_entry_close(dir);
-        cmd_append(cmd, "rm", "-r", BUILD_DIR);
-        if (!cmd_run(cmd)) {
+    if (file_exists(BUILD_DIR)) {
+        cmd_append(&cmd, "rm", "-r", BUILD_DIR);
+        if (!cmd_run(&cmd)) {
             nob_log(NOB_ERROR, "Unable to delete build directory");
             return false;
         }
-        cmd_append(cmd, "mkdir", BUILD_DIR);
-        if (!cmd_run(cmd)) {
+        cmd_append(&cmd, "mkdir", BUILD_DIR);
+        if (!cmd_run(&cmd)) {
             nob_log(NOB_ERROR, "Unable to create build directory");
             return false;
         }
@@ -74,35 +74,47 @@ bool clear_build_files(Cmd* cmd)
     return true;
 }
 
-bool compile_c_library(Cmd* cmd, const char* target, const char* compiler, const char* so_name, const char** source_files, size_t source_files_count)
+typedef struct {
+    const char* target;
+    const char* compiler;
+} TargetCompilerPair;
+
+typedef struct {
+    const char** items;
+    size_t count;
+    size_t capacity;
+} StringDA;
+
+bool compile_c_libraries(const TargetCompilerPair* targets, size_t targets_count, const char* so_name, const char** source_files, size_t source_files_count)
 {
-    // Create directory for specified target
-    cmd_append(cmd, "mkdir", "-p");
-    cmd_append(cmd, temp_sprintf("lib/%s", target));
-    if (!cmd_run(cmd)) {
-        nob_log(NOB_ERROR, "Unable to create directory for target %s", target);
-        return false;
-    }
+    for (size_t i = 0; i < targets_count; i++) {
+        // Create directory for specified target
+        cmd_append(&cmd, "mkdir", "-p");
+        cmd_append(&cmd, temp_sprintf("lib/%s", targets[i].target));
+        if (!cmd_run(&cmd)) {
+            nob_log(NOB_ERROR, "Unable to create directory for target %s", targets[i].target);
+            return false;
+        }
 
-    // Compile C code
-    cmd_append(cmd, compiler);
-    cmd_append(cmd, "-shared");
-    cmd_append(cmd, "-o", temp_sprintf("lib/%s/lib%s.so", target, so_name));
-    for (size_t i = 0; i < source_files_count; i++) {
-        cmd_append(cmd, temp_sprintf("%s/%s", csrc_dir, source_files[i]));
-    }
+        // Compile C code
+        cmd_append(&cmd, targets[i].compiler);
+        cmd_append(&cmd, "-shared");
+        cmd_append(&cmd, "-o", temp_sprintf("lib/%s/lib%s.so", targets[i].target, so_name));
+        for (size_t j = 0; j < source_files_count; j++) {
+            cmd_append(&cmd, source_files[j]);
+        }
 
-    if (!cmd_run(cmd)) {
-        nob_log(NOB_ERROR, "Unable to compile files for target %s", target);
-        return false;
+        if (!cmd_run(&cmd)) {
+            nob_log(NOB_ERROR, "Unable to compile files for target %s", targets[i].target);
+            return false;
+        }
     }
-
     return true;
 }
 
 typedef struct {
-    Cmd* cmd;
     const char* ext;
+    StringDA* files;
 } OnFileData;
 
 bool on_file(Walk_Entry entry)
@@ -111,32 +123,45 @@ bool on_file(Walk_Entry entry)
     if (entry.type == NOB_FILE_REGULAR) {
         String_View name = sv_from_cstr(entry.path);
         if (sv_ends_with_cstr(name, data->ext)) {
-            cmd_append(data->cmd, temp_strdup(entry.path));
+            da_append(data->files, temp_strdup(entry.path));
         }
     }
     return true;
 }
 
-bool cmd_append_by_extention(Cmd* cmd, const char* dir, const char* ext)
+bool find_files_by_extention(StringDA* files, const char* dir, const char* ext)
 {
     OnFileData data = {
-        .cmd = cmd,
         .ext = ext,
+        .files = files,
     };
     return walk_dir(dir, on_file, .post_order = false, .data = &data);
 }
 
-int main(int argc, char **argv)
+bool compile_resources(const char** resources, size_t resources_count)
 {
-    NOB_GO_REBUILD_URSELF(argc, argv);
-    get_paths();
+    cmd_append(&cmd, aapt2, "compile");
+    cmd_append(&cmd, "-o", "res.flata");
+    for (size_t i = 0; i < resources_count; i++) {
+        cmd_append(&cmd, temp_sprintf("%s/%s", prj_dir, resources[i]));
+    }
+    return cmd_run(&cmd);
+}
 
-    // TODO: extract each step in its own function
+bool link_unaligned()
+{
+    cmd_append(&cmd, aapt2, "link");
+    cmd_append(&cmd, "-o", "app.apk.unaligned");
+    cmd_append(&cmd, "-I", temp_sprintf("%s/android.jar", android_p_dir));
+    cmd_append(&cmd, "--manifest", temp_sprintf("%s/AndroidManifest.xml", prj_dir));
+    cmd_append(&cmd, "--java", "java");
+    cmd_append(&cmd, "--auto-add-overlay");
+    cmd_append(&cmd, "res.flata");
+    return cmd_run(&cmd);
+}
 
-    Cmd cmd = {0};
-
-    // Generating keystore for signing
-    const char* keystore = "keystore.jks";
+bool generate_keystore_if_not_exist(const char* keystore)
+{
     if (!file_exists(keystore)) {
         cmd_append(&cmd, "keytool");
         cmd_append(&cmd, "-genkey", "-v");
@@ -145,93 +170,113 @@ int main(int argc, char **argv)
         cmd_append(&cmd, "-keyalg", "RSA");
         cmd_append(&cmd, "-keysize", "2048");
         cmd_append(&cmd, "-validity", "10000");
-        if (!cmd_run(&cmd)) return 1;
+        return cmd_run(&cmd);
     }
+    return true;
+}
 
-    clear_build_files(&cmd);
-
-    if (!set_current_dir(BUILD_DIR)) return 1;
-
-    const char* c_src[] = {
-        "math.c",
-    };
-    const char* targets[] = {
-        "x86_64",
-        "armeabi-v7a",
-        "aarch64",
-    };
-    const char* compilers[] = {
-        x86_64_clang,
-        armv7a_clang,
-        aarch64_clang,
-    };
-    NOB_ASSERT(ARRAY_LEN(targets) == ARRAY_LEN(compilers));
-    const char* so_name = "math";
-
-    for (size_t i = 0; i < ARRAY_LEN(targets); i++) {
-        compile_c_library(&cmd, targets[i], compilers[i], so_name, c_src, ARRAY_LEN(c_src));
-    }
-
-    // Resources compilation
-    cmd_append(&cmd, aapt2, "compile");
-    cmd_append(&cmd, "-o", "res.flata");
-    cmd_append(&cmd, temp_sprintf("%s/res/mipmap-hdpi/ic_launcher.png", prj_dir));
-    if (!cmd_run(&cmd)) return 1;
-
-    // Unaligned apk linking
-    cmd_append(&cmd, aapt2, "link");
-    cmd_append(&cmd, "-o", "app.apk.unaligned");
-    cmd_append(&cmd, "-I", temp_sprintf("%s/android.jar", android_p_dir));
-    cmd_append(&cmd, "--manifest", temp_sprintf("%s/AndroidManifest.xml", prj_dir));
-    cmd_append(&cmd, "--java", "java");
-    cmd_append(&cmd, "--auto-add-overlay");
-    cmd_append(&cmd, "res.flata");
-    if (!cmd_run(&cmd)) return 1;
-
-    const char* kotlin_src[] = {
-        "MainActivity.kt",
-    };
-
-    // Compile kotlin code
+bool compile_kotlin(const char** kotlin_src, size_t kotlin_src_count)
+{
     cmd_append(&cmd, "kotlinc");
     cmd_append(&cmd, "-d", "classes");
     cmd_append(&cmd, "-cp", temp_sprintf("%s/android.jar", android_p_dir));
     cmd_append(&cmd, "-Xmetadata-version=2.2.0");
-    for (size_t i = 0; i < ARRAY_LEN(kotlin_src); i++) {
-        cmd_append(&cmd, temp_sprintf("%s/%s", src_dir, kotlin_src[i]));
+    for (size_t i = 0; i < kotlin_src_count; i++) {
+        cmd_append(&cmd, kotlin_src[i]);
     }
+    return cmd_run(&cmd);
+}
 
-    if (!cmd_run(&cmd)) return 1;
-
-    // Doing something with compiled kotlin code before adding to apk
+bool compile_dex()
+{
     cmd_append(&cmd, d8);
     cmd_append(&cmd, "--lib", temp_sprintf("%s/android.jar", android_p_dir));
     cmd_append(&cmd, "--output", ".");
-    if (!cmd_append_by_extention(&cmd, "classes", ".class")) return 1;
-    if (!cmd_run(&cmd)) return 1;
+    StringDA classes = {0};
+    if (!find_files_by_extention(&classes, "classes", ".class")) return false;
+    da_foreach(const char*, class, &classes) {
+        cmd_append(&cmd, *class);
+    }
+    return cmd_run(&cmd);
+}
 
-    // Adding resources and native libraries to apk
+bool add_dex_to_apk()
+{
     cmd_append(&cmd, aapt);
     cmd_append(&cmd, "add", "app.apk.unaligned");
     cmd_append(&cmd, "classes.dex");
-    for (size_t i = 0; i < ARRAY_LEN(targets); i++) {
-        cmd_append(&cmd, temp_sprintf("lib/%s/lib%s.so", targets[i], so_name));
-    }
-    if (!cmd_run(&cmd)) return 1;
+    return cmd_run(&cmd);
+}
 
-    // Aligning apk
+bool add_c_libraries_to_apk(const TargetCompilerPair* targets, size_t targets_count, const char* so_name)
+{
+    cmd_append(&cmd, aapt);
+    cmd_append(&cmd, "add", "app.apk.unaligned");
+    for (size_t i = 0; i < targets_count; i++) {
+        cmd_append(&cmd, temp_sprintf("lib/%s/lib%s.so", targets[i].target, so_name));
+    }
+    return cmd_run(&cmd);
+}
+
+bool align_apk()
+{
     cmd_append(&cmd, zipalign);
     cmd_append(&cmd, "-v", "-p", "4");
     cmd_append(&cmd, "app.apk.unaligned");
     cmd_append(&cmd, "app.apk");
-    if (!cmd_run(&cmd)) return 1;
+    return cmd_run(&cmd);
+}
 
-    // Signing apk
+bool sign_apk(const char* keystore)
+{
     cmd_append(&cmd, apksigner);
     cmd_append(&cmd, "sign", "--ks");
     cmd_append(&cmd, temp_sprintf("%s/%s", prj_dir, keystore));
     cmd_append(&cmd, "app.apk");
-    if (!cmd_run(&cmd)) return 1;
+    return cmd_run(&cmd);
+}
+
+int main(int argc, char **argv)
+{
+    NOB_GO_REBUILD_URSELF(argc, argv);
+    get_paths();
+
+    const char* keystore = "keystore.jks";
+
+    StringDA c_src = {0};
+    if (!find_files_by_extention(&c_src, csrc_dir, ".c")) return 1;
+
+    TargetCompilerPair targets[] = {
+        { .target = "x86_64", .compiler = x86_64_clang },
+        { .target = "armeabi-v7a", .compiler = armv7a_clang },
+        { .target = "aarch64", .compiler = aarch64_clang },
+    };
+    const char* so_name = "math";
+
+    StringDA kotlin_src = {0};
+    if (!find_files_by_extention(&kotlin_src, src_dir, ".kt")) return 1;
+
+    const char* resources[] = {
+        "res/mipmap-hdpi/ic_launcher.png",
+    };
+
+    if (!generate_keystore_if_not_exist(keystore)) return 1;
+    if (!clear_build_files()) return 1;
+
+    if (!set_current_dir(BUILD_DIR)) return 1;
+
+    if (!compile_c_libraries(targets, ARRAY_LEN(targets), so_name, c_src.items, c_src.count)) return 1;
+    if (!compile_resources(resources, ARRAY_LEN(resources))) return 1;
+    if (!link_unaligned()) return 1;
+
+    if (!compile_kotlin(kotlin_src.items, kotlin_src.count)) return 1;
+    if (!compile_dex()) return 1;
+
+    if (!add_dex_to_apk()) return 1;
+    if (!add_c_libraries_to_apk(targets, ARRAY_LEN(targets), so_name)) return 1;
+
+    if (!align_apk()) return 1;
+    if (!sign_apk(keystore))
 
     return 0;
 }
